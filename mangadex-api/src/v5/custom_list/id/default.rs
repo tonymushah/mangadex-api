063a3d9845f1fd3c57b9556dec89ff8234bc0e51
tonymushah <tonyramanantsalama@gmail.com>
2023-09-13 15:54:10 +0300

    added POST /list/{id}/default

--- mangadex-api/src/v5/custom_list/id/default.rs
+++ mangadex-api/src/v5/custom_list/id/default.rs
@@ -4,6 +4,8 @@ use crate::HttpClientRef;
 
 pub mod post;
 
+use post::SetDefaultCustomListBuilder;
+
 #[derive(Debug, Clone)]
 pub struct DefaultEndpoint {
     http_client: HttpClientRef,
@@ -15,7 +17,9 @@ impl DefaultEndpoint {
     pub fn new(http_client: HttpClientRef, id: Uuid) -> Self {
         Self { http_client, id }
     }
-    pub fn post(&self) {
-        todo!("implement the post method please")
+    pub fn post(&self) -> SetDefaultCustomListBuilder {
+        SetDefaultCustomListBuilder::default()
+            .http_client(self.http_client.clone())
+            .list_id(self.id)
     }
 }
--- mangadex-api/src/v5/custom_list/id/default/post.rs
+++ mangadex-api/src/v5/custom_list/id/default/post.rs
@@ -1,1 +1,88 @@
+use derive_builder::Builder;
+use mangadex_api_schema::NoData;
+use serde::Serialize;
+use uuid::Uuid;
 
+use crate::HttpClientRef;
+use mangadex_api_types::error::Result;
+
+#[cfg_attr(
+    feature = "deserializable-endpoint",
+    derive(serde::Deserialize, getset::Getters, getset::Setters)
+)]
+#[derive(Debug, Serialize, Clone, Builder)]
+#[serde(rename_all = "camelCase")]
+#[builder(
+    setter(into, strip_option),
+    pattern = "owned",
+    build_fn(error = "mangadex_api_types::error::BuilderError")
+)]
+#[cfg_attr(feature = "non_exhaustive", non_exhaustive)]
+pub struct SetDefaultCustomList {
+    /// This should never be set manually as this is only for internal use.
+    #[doc(hidden)]
+    #[serde(skip)]
+    #[builder(pattern = "immutable")]
+    #[cfg_attr(feature = "deserializable-endpoint", getset(set = "pub", get = "pub"))]
+    pub(crate) http_client: HttpClientRef,
+
+    /// CustomList ID.
+    #[serde(skip_serializing)]
+    pub list_id: Uuid,
+}
+
+endpoint! {
+    POST ("/list/{}/default", list_id),
+    #[body auth] SetDefaultCustomList,
+    #[discard_result] Result<NoData>
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+    use url::Url;
+    use uuid::Uuid;
+    use wiremock::matchers::{header, method, path_regex};
+    use wiremock::{Mock, MockServer, ResponseTemplate};
+
+    use crate::v5::AuthTokens;
+    use crate::{HttpClient, MangaDexClient};
+
+    #[tokio::test]
+    async fn set_default_custom_list_fires_a_request_to_base_url() -> anyhow::Result<()> {
+        let mock_server = MockServer::start().await;
+        let http_client = HttpClient::builder()
+            .base_url(Url::parse(&mock_server.uri())?)
+            .auth_tokens(AuthTokens {
+                session: "sessiontoken".to_string(),
+                refresh: "refreshtoken".to_string(),
+            })
+            .build()?;
+        let mangadex_client = MangaDexClient::new_with_http_client(http_client);
+
+        let custom_list_id = Uuid::new_v4();
+        let response_body = json!({
+            "result": "ok"
+        });
+
+        Mock::given(method("POST"))
+            .and(path_regex(r"/list/[0-9a-fA-F-]+/default"))
+            .and(header("Authorization", "Bearer sessiontoken"))
+            .and(header("Content-Type", "application/json"))
+            .respond_with(ResponseTemplate::new(200).set_body_json(response_body))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        mangadex_client
+            .custom_list()
+            .id(custom_list_id)
+            .default()
+            .post()
+            .build()?
+            .send()
+            .await?;
+
+        Ok(())
+    }
+}
