4a6ff3dd59cdb28ac2df0591d1b675fdf973e707
tonymushah <tonyramanantsalama@gmail.com>
2023-10-28 06:07:23 +0300

    Added `GET /client`

--- mangadex-api/src/v5/api_client.rs
+++ mangadex-api/src/v5/api_client.rs
@@ -1,3 +1,24 @@
 pub mod get;
 pub mod id;
 pub mod post;
+
+use get::ListClientsBuilder;
+
+use crate::HttpClientRef;
+use uuid::Uuid;
+
+create_endpoint_node! {
+    #[name] ApiClientEndpoint ApiClientEndpointMethods,
+    #[args] {
+        http_client: HttpClientRef,
+    },
+    #[methods] {
+        get() -> ListClientsBuilder;
+    }
+}
+
+impl ApiClientEndpointMethods for ApiClientEndpoint {
+    fn get(&self) -> ListClientsBuilder {
+        ListClientsBuilder::default().http_client(self.http_client.clone())
+    }
+}
