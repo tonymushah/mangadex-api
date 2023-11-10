aa24b0a2a7abec067896ec6eca8aaa84a6c14da5
tonymushah <tonyramanantsalama@gmail.com>
2023-11-10 14:50:51 +0300

    Added `oauth_manga_feed` example

--- mangadex-api/examples/oauth_manga_feed.rs
+++ mangadex-api/examples/oauth_manga_feed.rs
@@ -1,8 +1,12 @@
 use std::env::{var, VarError};
 
+use clap::Parser;
+
 use mangadex_api::MangaDexClient;
 use mangadex_api_schema::v5::oauth::ClientInfo;
-use mangadex_api_types::{Password, Username};
+use mangadex_api_types::{
+    Language, MangaFeedSortOrder, Password, ReferenceExpansionResource, Username,
+};
 
 pub type VarResult<T, E = std::io::Error> = Result<T, E>;
 
@@ -78,12 +82,31 @@ fn get_client_info_from_var() -> VarResult<ClientInfo> {
     })
 }
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
+#[derive(Parser)]
+#[clap(name = "MangaDex User Feed", about = "Fetch the user chapter feed")]
+struct Args {
+    /// Space-separated ISO 639-1 2-letter language code representation.
+    #[clap(default_value = "en")]
+    languages: Vec<Language>,
+    /// Start the results from the specified page number, starting from 1.
+    #[clap(short, long, default_value = "1")]
+    page: u32,
+    /// Set the maximum number of results to return.
+    #[clap(short, long, default_value = "10")]
+    limit: u32,
+}
+
+impl Args {
+    pub fn offset(&self) -> u32 {
+        get_page_offset(self.page, self.limit)
+    }
+}
+
+async fn run(arg: Args) -> anyhow::Result<()> {
     let client_info = get_client_info_from_var()?;
-    println!("client_info: {:?}", client_info);
+    //println!("client_info: {:?}", client_info);
     let user_info: UserInfos = TryFrom::try_from(PreUserInfos::new()?)?;
-    println!("user_info : {:?}", user_info);
+    //println!("user_info : {:?}", user_info);
 
     let mut client = MangaDexClient::default();
     client.set_client_info(&client_info).await?;
@@ -103,8 +126,42 @@ async fn main() -> anyhow::Result<()> {
 
     println!("Fetching your manga feed");
 
-    let feed = client.user().follows().manga().feed().get().send().await?;
+    let feed = client
+        .user()
+        .follows()
+        .manga()
+        .feed()
+        .get()
+        .limit(arg.limit)
+        .offset(arg.offset())
+        .translated_language(arg.languages)
+        .order(MangaFeedSortOrder::ReadableAt(
+            mangadex_api_types::OrderDirection::Descending,
+        ))
+        .include(ReferenceExpansionResource::Manga)
+        .send()
+        .await?;
     println!("Fetched");
-    println!("{:?}", feed);
+    println!("{}", serde_json::to_string_pretty(&feed)?);
     Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+
+    if let Err(e) = run(args).await {
+        use std::process;
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+/// Calculate the offset needed to view the page results.
+fn get_page_offset(page: u32, limit: u32) -> u32 {
+    if page == 0 || limit == 0 {
+        return 0;
+    }
+
+    limit * (page - 1)
 }
