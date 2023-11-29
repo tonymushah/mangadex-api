62bb92fe67690df3843a91a630b5fcab8b859ba3
tonymushah <tonyramanantsalama@gmail.com>
2023-11-29 07:20:09 +0300

    Fixed and Removed `TODO` in `POST /list`

--- mangadex-api/src/v5/custom_list/post.rs
+++ mangadex-api/src/v5/custom_list/post.rs
@@ -91,7 +91,7 @@ mod tests {
     use serde_json::json;
     use url::Url;
     use uuid::Uuid;
-    use wiremock::matchers::{header, method, path};
+    use wiremock::matchers::{body_json, header, method, path};
     use wiremock::{Mock, MockServer, ResponseTemplate};
 
     use crate::v5::AuthTokens;
@@ -111,7 +111,7 @@ mod tests {
 
         let custom_list_id = Uuid::new_v4();
         let custom_list_name: String = Name().fake();
-        let _expected_body = json!({
+        let expected_body = json!({
             "name": custom_list_name,
             "version": 1
         });
@@ -134,8 +134,7 @@ mod tests {
             .and(path(r"/list"))
             .and(header("Authorization", "Bearer sessiontoken"))
             .and(header("Content-Type", "application/json"))
-            // TODO: Make the request body check work.
-            // .and(body_json(expected_body))
+            .and(body_json(expected_body))
             .respond_with(ResponseTemplate::new(200).set_body_json(response_body))
             .expect(1)
             .mount(&mock_server)
