129336e81396c5d1ab79b89292255cc65b318ffa
tonymushah <tonyramanantsalama@gmail.com>
2023-12-07 11:05:22 +0300

    Added `#[cfg_attr(feature = "serialize", derive(serde::Serialize))]` in oauth input types

--- mangadex-api-input-types/src/oauth/login.rs
+++ mangadex-api-input-types/src/oauth/login.rs
@@ -5,9 +5,10 @@ use mangadex_api_schema::v5::oauth::OAuthTokenResponse;
 #[cfg(feature = "mangadex-api-resolver")]
 use mangadex_api_types::{error::Result, Password, Username};
 
-#[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
+#[derive(serde::Deserialize, Debug, Clone)]
 #[cfg_attr(feature = "specta", derive(specta::Type))]
 #[cfg_attr(feature = "async-graphql", derive(async_graphql::InputObject))]
+#[cfg_attr(feature = "serialize", derive(serde::Serialize))]
 pub struct OAuthLoginParams {
     pub username: String,
     pub password: String,
