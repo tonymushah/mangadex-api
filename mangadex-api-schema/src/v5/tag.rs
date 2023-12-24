50757a80b59f09a2c466a11b3966ef6db5110143
tonymushah <tonyramanantsalama@gmail.com>
2023-12-24 16:21:41 +0300

    Added TagEnum Coversion to `ApiObjectNoRelationship<TagAttributes>`

--- mangadex-api-schema/src/v5/tag.rs
+++ mangadex-api-schema/src/v5/tag.rs
@@ -1,7 +1,12 @@
-use mangadex_api_types::TagGroup;
+use std::collections::HashMap;
+
+use mangadex_api_types::{Language, RelationshipType, Tag, TagGroup};
 use serde::Deserialize;
 
-use crate::v5::{localizedstring_array_or_map, LocalizedString};
+use crate::{
+    v5::{localizedstring_array_or_map, LocalizedString},
+    ApiObjectNoRelationships,
+};
 
 #[derive(Clone, Debug, Deserialize)]
 #[cfg_attr(feature = "serialize", derive(serde::Serialize))]
@@ -14,4 +19,21 @@ pub struct TagAttributes {
     pub description: LocalizedString,
     pub group: TagGroup,
     pub version: u32,
+}
+
+impl From<Tag> for ApiObjectNoRelationships<TagAttributes> {
+    fn from(value: Tag) -> Self {
+        let mut name: HashMap<Language, String> = HashMap::new();
+        name.insert(Language::English, value.to_string());
+        Self {
+            id: value.into(),
+            type_: RelationshipType::Tag,
+            attributes: TagAttributes {
+                name,
+                description: HashMap::default(),
+                group: value.into(),
+                version: 1,
+            },
+        }
+    }
 }
