478b43b4e9f1ac35a7a552d9514701c644697eea
tonymushah <tonyramanantsalama@gmail.com>
2024-03-29 21:36:31 +0300

    Changed `UploadRequiredApproval::requires_approval` `bool` type to `Option<bool>` and added `UploadRequiredApproval::is_manga_not_found`

--- mangadex-api-schema/src/v5/upload_required_approval.rs
+++ mangadex-api-schema/src/v5/upload_required_approval.rs
@@ -13,21 +13,28 @@ use crate::FromResponse;
 pub struct UploadRequiredApproval {
     #[serde(default)]
     pub result: ResultType,
-    pub requires_approval: bool,
+    #[serde(default)]
+    pub requires_approval: Option<bool>,
 }
 
 impl From<bool> for UploadRequiredApproval {
     fn from(value: bool) -> Self {
         Self {
             result: Default::default(),
-            requires_approval: value,
+            requires_approval: Some(value),
         }
+    }
+}
+
+impl UploadRequiredApproval {
+    pub fn is_manga_not_found(&self) -> bool {
+        self.requires_approval.is_none()
     }
 }
 
 impl From<UploadRequiredApproval> for bool {
     fn from(value: UploadRequiredApproval) -> Self {
-        value.requires_approval
+        value.requires_approval.unwrap_or_default()
     }
 }
 
