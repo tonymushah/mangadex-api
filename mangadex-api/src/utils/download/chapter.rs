576208a1d76f1e82407b1a602db84a72edd425a0
tonymushah <tonyramanantsalama@gmail.com>
2025-05-11 19:16:12 +0300

    using buff write

--- mangadex-api/src/utils/download/chapter.rs
+++ mangadex-api/src/utils/download/chapter.rs
@@ -139,9 +139,10 @@ impl ChapterDownload {
 mod tests {
     use crate::{utils::download::chapter::DownloadMode, MangaDexClient};
     use anyhow::Result;
+    use bytes::Buf;
     use std::{
         fs::{create_dir_all, File},
-        io::Write,
+        io::{BufWriter, Write},
     };
     use tokio::pin;
     use tokio_stream::StreamExt;
@@ -167,7 +168,11 @@ mod tests {
             if let Ok(bytes) = bytes_ {
                 let mut file: File =
                     File::create(format!("{}{}/{}", output_dir, chapter_id, filename))?;
-                file.write_all(&bytes)?
+                {
+                    let mut buffer = BufWriter::new(&mut file);
+                    std::io::copy(&mut bytes.reader(), &mut buffer)?;
+                    buffer.flush()?;
+                }
             };
         }
         Ok(())
@@ -175,7 +180,7 @@ mod tests {
 
     /// It's from this manga called [`Keiken Zumi na Kimi to, Keiken Zero na Ore ga, Otsukiai Suru Hanashi`](https://mangadex.org/title/1c8f0358-d663-4d60-8590-b5e82890a1e3/keiken-zumi-na-kimi-to-keiken-zero-na-ore-ga-otsukiai-suru-hanashi)
     ///
-    /// [Chapter 13 English](https://mangadex.org/chapter/84d76ccb-4068-4e7d-873b-c2bf0fefde65) by [`Galaxy Degen Scans`](https://mangadex.org/group/ab24085f-b16c-4029-8c05-38fe16592a85/galaxy-degen-scans)
+    /// [Chapter 30 English](https://mangadex.org/chapter/84d76ccb-4068-4e7d-873b-c2bf0fefde65) by [`Galaxy Degen Scans`](https://mangadex.org/group/ab24085f-b16c-4029-8c05-38fe16592a85/galaxy-degen-scans)
     #[tokio::test]
     async fn download_chapter_with_streams() -> Result<()> {
         let output_dir = "./test-outputs/";
@@ -195,7 +200,11 @@ mod tests {
             if let Ok(bytes) = bytes_ {
                 let mut file: File =
                     File::create(format!("{}{}/{}", output_dir, chapter_id, filename))?;
-                file.write_all(&bytes)?
+                {
+                    let mut buffer = BufWriter::new(&mut file);
+                    std::io::copy(&mut bytes.reader(), &mut buffer)?;
+                    buffer.flush()?;
+                }
             }
         }
         Ok(())
@@ -203,7 +212,7 @@ mod tests {
 
     /// It's from this manga called [`Keiken Zumi na Kimi to, Keiken Zero na Ore ga, Otsukiai Suru Hanashi`](https://mangadex.org/title/1c8f0358-d663-4d60-8590-b5e82890a1e3/keiken-zumi-na-kimi-to-keiken-zero-na-ore-ga-otsukiai-suru-hanashi)
     ///
-    /// [Chapter 30 English](https://mangadex.org/chapter/250f091f-4166-4831-9f45-89ff54bf433b) by [`Galaxy Degen Scans`](https://mangadex.org/group/ab24085f-b16c-4029-8c05-38fe16592a85/galaxy-degen-scans)
+    /// [Chapter 13 English](https://mangadex.org/chapter/250f091f-4166-4831-9f45-89ff54bf433b) by [`Galaxy Degen Scans`](https://mangadex.org/group/ab24085f-b16c-4029-8c05-38fe16592a85/galaxy-degen-scans)
     #[tokio::test]
     async fn download_chapter_with_streams_and_checker() -> Result<()> {
         let output_dir = "./test-outputs/";
@@ -248,7 +257,11 @@ mod tests {
             if let Ok(bytes) = bytes_ {
                 let mut file: File =
                     File::create(format!("{}{}/{}", output_dir, chapter_id, filename))?;
-                file.write_all(&bytes)?;
+                {
+                    let mut buffer = BufWriter::new(&mut file);
+                    std::io::copy(&mut bytes.reader(), &mut buffer)?;
+                    buffer.flush()?;
+                }
                 println!("Downloaded {filename}");
             } else {
                 println!("Skipped {filename}");
